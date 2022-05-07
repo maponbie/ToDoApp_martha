44c551ae29894f2d80db4992d1aabdfadacde479
maponbie <martha.aponbiede@valpo.edu>
2022-05-07 11:33:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/db/TaskContract.java
@@ -0,0 +1,17 @@
+package com.example.todoapp.db;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+
+    public static final String DB_NAME = "com.example.todoapp.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+
+        public static final String TABLE = "tasks";
+        public static final String COL_TASK_TITLE = "title";
+    }
+
+
+}
---
+++ app/src/main/java/com/example/todoapp/db/TaskDbHelper.java
@@ -0,0 +1,30 @@
+package com.example.todoapp.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TaskDbHelper extends SQLiteOpenHelper {
+
+    public TaskDbHelper(Context context) {
+        super(context, TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TaskContract.TaskEntry.TABLE + " ( " +
+                TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TaskContract.TaskEntry.COL_TASK_TITLE + " TEXT NOT NULL);";
+
+        db.execSQL(createTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TaskContract.TaskEntry.TABLE);
+        onCreate(db);
+
+    }
+}
